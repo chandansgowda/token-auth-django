3ba18d55c9821a04799c67b22cf1d9a7cae1e1a3
chandansgowda <chandansuresh007@gmail.com>
2023-08-21 13:17:54 +0530

    feat: user auth API

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'core',
+    'user'
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -19,11 +19,12 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
-         name='api-docs',)
+         name='api-docs',),
+    path('api/user/', include('user.urls')),
 ]
